0324275d66b5cd74f07d3f13991a0ec562344c73
Yuvraaj Narula <49155095+yuvraajnarula@users.noreply.github.com>
2022-11-16 05:29:55 +0000

    base add admin base map tracker

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,18 @@
 const router = require('express').Router()
-
+const User = require('../models/userSchema')
 router.get('/',(req,res)=>{
-    res.render('index')
-})
-router.post('/crack',(req,res)=>{
-    console.log(req.body)
+    const UserData = User.find({})
+    res.render('admin/index',{
+        map : process.env.MAP
+    })
 })
 
+router.get('/user/fetch',(req,res)=>{
+    User.find({}).then((users) => {
+        res.json({users})
+    }).catch((err) => console.log(err))
+})
+router.get('/userInfo/:id', (req,res)=>{
+    Users.findById(req.params.id)
+})
 module.exports = router
